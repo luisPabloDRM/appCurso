497b542dd900c93fda498ea6c38c7a987265d1dd
Luis Pablo <lp.delriom@gmail.com>
2024-02-15 08:48:53 +0100

    App arch done

---
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping-list-edit.component.html',
+  styleUrl: './shopping-list-edit.component.css'
+})
+export class ShoppingListEditComponent {
+
+}
