6fb0ab054fc4032f59b32aa0edb5bda6a6059d42
Luis Pablo <lp.delriom@gmail.com>
2024-02-15 16:51:05 +0100

    Giving some life to the components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { Ingredient } from '../recipe-book/models/ingredient';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [ShoppingListEditComponent],
+  imports: [ShoppingListEditComponent, CommonModule],
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    { name: 'Apples', amount: 5 },
+    { name: 'Tomatoes', amount: 10 },
+  ];
 
 }
