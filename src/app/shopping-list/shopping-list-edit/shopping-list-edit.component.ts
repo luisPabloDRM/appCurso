6aed726274e43f75b4c6f98c05eb3b2dd5e28226
lpdelrio-duonex <luispablo.delrio@duonex.com>
2024-02-19 14:49:52 +0100

    Create ingredients inputs by local reference

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../recipe-book/models/ingredient';
 
 @Component({
   selector: 'app-shopping-list-edit',
   standalone: true,
   imports: [],
   templateUrl: './shopping-list-edit.component.html',
-  styleUrl: './shopping-list-edit.component.css'
+  styleUrl: './shopping-list-edit.component.css',
 })
 export class ShoppingListEditComponent {
+  @ViewChild('nameInput', { static: true }) nameInputRef:| ElementRef | undefined;
+  @ViewChild('amountInput', { static: true }) amountInputRef:| ElementRef | undefined;
 
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() {}
+
+  addIngredients() {
+    const ingName = this.nameInputRef!.nativeElement.value;
+    const ingAmount = this.amountInputRef!.nativeElement.value;
+    const newIngredient: Ingredient = { name: ingName, amount: ingAmount };
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
