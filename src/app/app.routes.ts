31d6b2f68601a8f95c07de168e73328f61d87da2
Luis Pablo <lp.delriom@gmail.com>
2024-02-22 13:49:49 +0100

    Setting the router nav into my app 1.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component'; 
 import { HomeComponent } from './home/home.component';
+import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 
 
@@ -16,6 +18,16 @@ export const routes: Routes = [
     },
     {
         path :'recipes',
-        component: RecipeBookComponent
+        component: RecipeBookComponent,
+        children: [
+            {
+                path: '',
+                component: RecipeStartComponent
+            },
+            {
+                path: ':id',
+                component: RecipeDetailComponent
+            }
+        ]
     }
 ]
