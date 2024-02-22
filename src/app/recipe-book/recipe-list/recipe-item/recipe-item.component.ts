746296e9a3e122ae13480c56e74a060d6a36f4b6
Luis Pablo <lp.delriom@gmail.com>
2024-02-22 15:45:04 +0100

    Set link when we´re ponting to a recipe

--- src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipe } from '../../models/recipe';
+import { Router } from 'express';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.css',
 })
