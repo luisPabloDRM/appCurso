dac3e00d73d9d54f3869b200a109b020f672d4c2
Luis Pablo <lp.delriom@gmail.com>
2024-02-22 16:33:44 +0100

    Adding some files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,30 +4,18 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { HomeComponent } from './home/home.component';
 import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
 
 
 
 export const routes: Routes = [
-    {
-        path :'',
-        component: HomeComponent
-    },
-    {
-        path :'shoppingList',
-        component: ShoppingListComponent
-    },
-    {
-        path :'recipes',
-        component: RecipeBookComponent,
-        children: [
-            {
-                path: '',
-                component: RecipeStartComponent
-            },
-            {
-                path: ':id',
-                component: RecipeDetailComponent
-            }
+    {path :'',component: HomeComponent},
+    {path :'shoppingList',component: ShoppingListComponent},
+    {path :'recipes',component: RecipeBookComponent, children: [
+            {path: '', component: RecipeStartComponent},
+            {path: 'new',component: RecipeEditComponent},
+            {path: ':id', component: RecipeDetailComponent},
+            {path: ':id/edit',component: RecipeEditComponent}
         ]
     }
 ]
