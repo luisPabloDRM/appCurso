055685831eed5f613a98cf6995b449ef89cf1d2f
lpdelrio-duonex <luispablo.delrio@duonex.com>
2024-02-27 08:37:09 +0100

    Modifications in appRoutes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,22 @@
 import { Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component'; 
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { HomeComponent } from './home/home.component';
 import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
 
-
-
 export const routes: Routes = [
-    {path :'',component: HomeComponent},
-    {path :'shoppingList',component: ShoppingListComponent},
-    {path :'recipes',component: RecipeBookComponent, children: [
-            {path: '', component: RecipeStartComponent},
-            {path: 'new',component: RecipeEditComponent},
-            {path: ':id', component: RecipeDetailComponent},
-            {path: ':id/edit',component: RecipeEditComponent}
-        ]
-    }
-]
+  { path: '', component: HomeComponent },
+  { path: 'shoppingList', component: ShoppingListComponent },
+  {
+    path: 'recipes',
+    component: RecipeBookComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
+    ],
+  },
+];
