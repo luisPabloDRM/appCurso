9edf46a120def7b0dc7da3bf48b2906453bdc4f5
lpdelrio-duonex <luispablo.delrio@duonex.com>
2024-02-28 16:32:07 +0100

    Adding xplain file

---
+++ customPipes.pipe.ts
@@ -0,0 +1,22 @@
+// Código de ejemplo para hacer una Pipe de filtrado 
+
+import { Pipe, PipeTransform } from '@angular/core'
+
+@Pipe({
+    name: 'filter'
+})
+export class FilterPipe {
+    transform(value: any , filterString : string, propName: string ){
+        if (value.lenght === 0 || filterString === '') {
+            return value;
+        }
+
+        const resultArray = [];
+        for( const item of value ){
+            if (item[propName] === filterString) {
+                resultArray.push(item)
+            }
+        }
+        return resultArray
+    }
+}
