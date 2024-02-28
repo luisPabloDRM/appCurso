a52cc0de56bdd8a0f5bcdd2d355fe390dc172482
lpdelrio-duonex <luispablo.delrio@duonex.com>
2024-02-28 09:05:25 +0100

    set some validators to form fields, and to the buttons

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../services/recipe.service';
 import { CommonModule } from '@angular/common';
@@ -33,6 +33,18 @@ export class RecipeEditComponent {
     console.log(this.newRecipe);
   }
 
+  onAddIngredient(){
+    (<FormArray>this.newRecipe.get('ingredients')).push(
+      new FormGroup({
+        'name' : new FormControl(null, Validators.required),
+        'amount': new FormControl(null,[
+                Validators.required,
+                Validators.pattern(/^[1-9] + [0-9]*$/),
+              ])
+      })
+    )
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
@@ -44,12 +56,15 @@ export class RecipeEditComponent {
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
-      if (recipe['ingredients']){
+      if (recipe['ingredients']) {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount),
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]\d*$/),
+              ]),
             })
           );
         }
@@ -57,9 +72,9 @@ export class RecipeEditComponent {
     }
 
     this.newRecipe = new FormGroup({
-      name: new FormControl(recipeName),
-      imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription),
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
       ingredients: recipeIngredients,
     });
   }
