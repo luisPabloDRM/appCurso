d30b208832f1a67ea8e36dbcbe3b520f38138977
lpdelrio-duonex <luispablo.delrio@duonex.com>
2024-03-05 16:43:20 +0100

    Adding some logic to avoid possible bugs

--- src/app/recipe-book/services/recipe.resolver.ts
+++ src/app/recipe-book/services/recipe.resolver.ts
@@ -2,8 +2,16 @@ import { ResolveFn } from "@angular/router";
 import { Recipe } from "../models/recipe";
 import { inject } from "@angular/core";
 import { DataStorageService } from "../../shared/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 export const recipeFetchDataResolve : ResolveFn<any> = (route) => {
     const dataStorage = inject(DataStorageService);
-    return dataStorage.fetchingRecipes()
+    const recipeService = inject(RecipeService)
+    const recipes = recipeService.getRecipes()
+
+    if( recipes.length === 0){
+        return dataStorage.fetchingRecipes()
+    }else {
+        return recipes;
+    }
 }
