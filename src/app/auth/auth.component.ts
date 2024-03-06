f6086406d4ca38d7206a069feb68e17c50f1eacd
lpdelrio-duonex <luispablo.delrio@duonex.com>
2024-03-06 12:21:20 +0100

    Refactor login funct on component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from './auth.service';
 import { error } from 'console';
 import { LoadingSpinner } from '../shared/loading-spinner/loading-spinner';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -15,10 +17,9 @@ import { CommonModule } from '@angular/common';
 export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
-  error! :string;
+  error!: string;
 
-  constructor( private authService : AuthService){}
-
+  constructor(private authService: AuthService) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -30,23 +31,27 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>
 
     this.isLoading = true;
-    if( this.isLoginMode){
-      //...
-    }else {
-        this.authService.signUp(email, password).subscribe(
-          resData => {
-            console.log(resData)
-            this.isLoading = false;
-        }, 
-          error => {
-            console.error(error);
-            this.isLoading = false;
-            this.error = 'An error ocurred!'
-        }
-      );
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password)
+
+    } else {
+      authObs = this.authService.signUp(email, password)
     }
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    )
 
     form.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { error } from "console";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-interface AuthResponseData {
-    kind:string;
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
-}
+ export interface AuthResponseData {
+            kind?:string;
+            idToken: string;
+            email: string;
+            refreshToken: string;
+            expiresIn: string;
+            localId: string; 
+            registered? : boolean;
+        }
+
+
 
 @Injectable({providedIn: 'root'})
 export class AuthService{
@@ -18,11 +24,32 @@ export class AuthService{
 
     signUp(email: string , password: string){
        return this.httpClient.post<AuthResponseData>(
-          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key= AIzaSyBThLRIT30tQGgd-4rC1XP0mqhNSaNKf5M ', 
+          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key= AIzaSyBThLRIT30tQGgd-4rC1XP0mqhNSaNKf5M', 
           {
             email:email,
             password:password,
             returnSecureToken: true
+          }
+        ).pipe(catchError( errorRes => {
+            let errorMessage = 'An unknow error ocurred';
+            if (!errorRes.error || !errorRes.error.error){
+                return throwError(errorMessage)
+            }
+            switch (errorRes.error.error.message) {
+              case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already!';
+            }
+            return throwError(errorMessage)
+        }))
+    }
+
+    login(email: string , password: string){
+        return this.httpClient.post<AuthResponseData>(
+          'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBThLRIT30tQGgd-4rC1XP0mqhNSaNKf5M',
+          {
+            email: email,
+            password: password,
+            returnSecureToken: true,
           }
         );
     }
